ef2609e169efaa2710de8aba8b0ff146c16feccf
unknown <benji.crespin@gmail.com>
2014-12-09 09:49:52 +0100

    first commit

---
+++ app/src/main/java/org/esiea/dagniau/crespin/musicseverywhere/CreateNewAccount.java
@@ -0,0 +1,15 @@
+package org.esiea.dagniau.crespin.musicseverywhere;
+
+/**
+ * Created by Benjamin Crespin et Marie Dagniau on 08/12/2014.
+ */
+public class CreateNewAccount {
+
+    /*
+    * Vérife Email
+    * MDP 6 caractères
+    * Vérif Pseudo
+    *
+    * */
+
+}
---
+++ app/src/main/java/org/esiea/dagniau/crespin/musicseverywhere/MainActivity.java
@@ -0,0 +1,65 @@
+package org.esiea.dagniau.crespin.musicseverywhere;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Benjamin Crespin et Marie Dagniau on 08/12/2014.
+ */
+public class MainActivity extends Activity{
+    MediaPlayer mediaPlayer;
+    String urlMusic = "/storage/extSdCard/Music/Workout Motivation Music.mp3";
+    TextView titleSong = (TextView) findViewById(R.id.titleSong);
+    List<String> favoriteMusics = new ArrayList<String>();
+    List<String> deleteMusics = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Context context = this;
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.setDataSource(urlMusic);
+            mediaPlayer.prepare();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void playMusic(View v){
+      mediaPlayer.start();
+      //titleSong.setText(title);// Changer titre grâce aux fonctions Deezer et Spotify
+    }
+
+    public void pauseMusic(View v){
+        mediaPlayer.pause();
+    }
+
+    public void likeMusic(View v){
+        favoriteMusics.add(titleSong.getText().toString());
+    }
+
+
+    public void dislikeMusic(View v){
+        favoriteMusics.add(titleSong.getText().toString());
+    }
+
+}
